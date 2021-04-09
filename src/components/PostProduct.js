1a96faf0b9327e91154ca436c6aacc3ccfa917b0
i'mfebri <iamfebriadji@gmail.com>
2021-04-09 21:30:47 +0700

    menambahkan props formValue

--- src/components/PostProduct.js
+++ src/components/PostProduct.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function PostProducts({ formSubmit, formChange }) {
+export default function PostProducts({ formSubmit, formChange, formValue }) {
   return (
     <div className="products-post">
       <form onSubmit={ formSubmit }>
@@ -8,11 +8,13 @@ export default function PostProducts({ formSubmit, formChange }) {
           name = "name"
           placeholder = "masukan nama product"
           onChange = { formChange }
+          value = { formValue.name } // value berasal dari state postProducts
         />
         <input 
           name = "description"
           placeholder = "masukan deskripsi singkat tentang product anda"
           onChange = { formChange }
+          value = { formValue.description } // value berasal dari state postProducts
         />
         <button type="submit">Submit Product</button>
       </form>
