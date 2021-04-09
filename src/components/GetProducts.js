caf7e239ea8c5866404205d388265da28c442897
i'mfebri <iamfebriadji@gmail.com>
2021-04-10 01:47:34 +0700

    membuat button untuk melakukan aksi hapus product

--- src/components/GetProducts.js
+++ src/components/GetProducts.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-export default function GetProducts({ products }) {
+export default function GetProducts({ products, deleteProduct }) {
   return (
     <div className="products-card">
+      <button 
+        value = { products.id }
+        onClick = { deleteProduct }
+      > Delete Product
+      </button>
       <h2 className="name">{ products.name }</h2>
       <p className="description">{ products.description }</p>
       <p>{ products.date }</p>
